8d4f410cad252f906bf4f0a655388fa0471ab905
Leox343 <leo.xu6@student.tdsb.on.ca>
2024-10-15 18:40:37 +0000

    hi

--- PP2_2.py
+++ PP2_2.py
@@ -1,11 +1,38 @@
-
-
+'''
+lesson 2.2
+created by leo xu
+last edited on oct 15
+'''
 def q1(): 
   #Write Assignment code here
-
-
+  ans = input("Input an integer: ")
+  if int(ans) == 5: 
+    print("The number is Five")
+  else:
+    print("The number is not Five")
 def q2(): 
   #Write Assignment code here
+  ans2 = input("Input a number: ")
+  if bool(ans2) < 0: 
+    print("Negative")
+  else:
+    print("Positive")
+def q3():
+  #Write Assignment code here
+  ans3 = input("Input an integer: ")
+  ans4 = int(ans3) % 2
+  if ans4 == 0:
+    print("Even")
+  else:
+    print("Odd")
+def q4():
+  #Write Assignment code here
+  ans4 = input('Type "Hello": ')
+  if ans4 == "Hello":
+    print("The word is Hello")
+  else:
+    print("The word is not Hello")
+  
 
 
 
@@ -14,3 +41,5 @@ def q2():
 
 q1()
 q2()
+q3()
+q4()
